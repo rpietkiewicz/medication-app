ec1b376223a09ca82df4e6f28ea406406370f564
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2020-04-13 19:58:34 +1000

    Add edit method

--- medication-app/src/App.js
+++ medication-app/src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
 
   handleInput(e) {
@@ -53,7 +54,19 @@ class App extends React.Component {
       this.setState({
         items:filteredItems
       })
-    }
+  }
+
+  setUpdate(text, key) {
+    const items = this.state.items;
+    items.map(item => {
+      if(item.key===key) {
+        item.text=text;
+      }
+    })
+    this.setState({
+    items: items
+    })
+  }
 
   render() {
     return (
@@ -67,7 +80,8 @@ class App extends React.Component {
           </form>
         </header>
         <ListItems items = {this.state.items}
-        deleteItem = {this.deleteItem}></ListItems>
+        deleteItem = {this.deleteItem}
+        setUpdate={this.setUpdate}></ListItems>
        </div>
     );
   }
