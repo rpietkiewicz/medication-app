7d58101ecd773f1056aa2deb07e03137e5d8970a
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2020-04-13 12:34:55 +1000

    Add methods for handle input and add item

--- medication-app/src/App.js
+++ medication-app/src/App.js
@@ -3,12 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+      currentItem: {
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault(); //prevent app from refreshing when Dodaj button is clicked
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItems];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
        <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Dodaj leki.."/>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Dodaj leki.."
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}/>
             <button type="submit">Dodaj</button>
           </form>
         </header>
